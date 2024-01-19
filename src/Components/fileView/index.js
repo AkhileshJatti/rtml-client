e99e2bd86c8c7259eecba5d7f21a1dfbd0c58174
akhilesh <akhilesh.jatti@solvendo.io>
2024-01-19 12:09:59 +0530

    fixed file view

--- src/Components/fileView/index.js
+++ src/Components/fileView/index.js
@@ -4,12 +4,14 @@ const FileView = (props) => {
     const {
         fileDataShow,
     } = props
+
+    console.log(fileDataShow)
     return (
         <div className="container p-10 overflow-auto">
             <div className="p-10 bg-viewBG flex flex-col text-center bg-opacity-40">
                 {fileDataShow?.map((lineItem) => {
-                    if(typeof (lineItem.str)==="number"){
-                        return (<>Please Select a File</>)
+                    if((lineItem.str)===""){
+                        return (<>Please Select a File from the Sidebar</>)
                     }
                     return (lineItem?.str?.split('\n').map((item) => <div id={lineItem?.id} className={lineItem?.highLight ? "bg-blue-100 scroll-mt-10" : "bg-none"}>{item}</div>
                     ))
